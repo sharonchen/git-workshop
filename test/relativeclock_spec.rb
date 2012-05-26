465e394c099f10a80776ff46a5752bf539c63753
sharonchen <sc.betterintime@gmail.com>
2012-05-26 15:20:56 +0800

    Update test/relativeclock_spec.rb

--- test/relativeclock_spec.rb
+++ test/relativeclock_spec.rb
@@ -1,12 +1,14 @@
-require 'singapore_relative_clock'
+require 'singapore_relative_clock' 
 
 describe SingaporeRelativeClock do
 	before(:all) do
+		SGtime = Time.new
 		@clock = SingaporeRelativeClock.new
 	end
 
-	it "should..." do
-
+	it "should give -2 hours to Sydney" do
+		@clock.timeDifferencetTo("Sydney").should===-2
+		#SYDtime = SGtime - 2.Hours
 	end
-
 end
+
